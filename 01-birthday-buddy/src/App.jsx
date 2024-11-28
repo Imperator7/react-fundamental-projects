84486bba0128e59feb3a611147beded618c70e6e
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2024-11-28 15:34:01 +0700

    final version of the project

--- 01-birthday-buddy/src/App.jsx
+++ 01-birthday-buddy/src/App.jsx
@@ -1,4 +1,19 @@
+import { useState } from 'react'
+import List from './List'
+import ClearBtn from './ClearBtn'
+
+import data from './data'
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
-export default App;
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className="container">
+        <h1>{people.length} Birthdays Today</h1>
+        <List people={people} />
+        <ClearBtn setPeople={setPeople} />
+      </section>
+    </main>
+  )
+}
+export default App
